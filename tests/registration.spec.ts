89d07ace36f932967f00b87394bfc916cc676f7b
Graeme Stewart <graemes88@hotmail.com>
2025-04-16 16:14:43 +0100

    Use before hooks

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -2,21 +2,18 @@ import { test, expect } from '@playwright/test';
 import { RegistrationPage } from '../page-objects/registration';
 let registrationPage: RegistrationPage
 
-// test.beforeEach(async ({ page }) => {
-//     const registrationPage = new RegistrationPage(page)
-//     await registrationPage.goto()
-// })
+test.beforeEach(async ({ page }) => {
+    registrationPage = new RegistrationPage(page);
+    await registrationPage.goto()
+})
 
 test('has title', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page)
-    await registrationPage.goto()
   // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Register/);
 });
 
 test('this username already exists error', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page)
-    await registrationPage.goto()
+    // TODO users are wiped every day so test needs setup/checked
     await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
     await expect(registrationPage.duplicateerror).toBeVisible();
 });
