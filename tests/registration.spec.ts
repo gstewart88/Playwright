db8720955e6e7fde5070f73553407e46d99dbb07
Graeme Stewart <graemes88@hotmail.com>
2025-04-17 12:01:34 +0100

    Use after hooks

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import { RegistrationPage } from '../page-objects/registration';
 let registrationPage: RegistrationPage
 
@@ -7,13 +7,21 @@ test.beforeEach(async ({ page }) => {
     await registrationPage.goto()
 })
 
+test.afterEach(async ({ request }) => {
+    const response = await request.post('http://localhost:8080/parabank/services/bank/cleanDB');
+    //When using WebKit on macOS, accessing localhost will not pick up client certificates. You can make it work by replacing localhost with local.playwright.
+    expect(response.status()).toBe(204);
+});
+
 test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Register/);
 });
 
 test('this username already exists error', async ({ page }) => {
-    // TODO users are wiped every day so test needs setup/checked
+    // Create new user twice to create a duplicate
+    await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
+    await registrationPage.goto()
     await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
     await expect(registrationPage.duplicateerror).toBeVisible();
 });
