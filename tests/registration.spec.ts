5d362c46a68bf4c42f864eecc57f8c9338bf9619
Graeme Stewart <graemes88@hotmail.com>
2025-04-17 12:23:29 +0100

    Add required field tests for registration page

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -18,6 +18,49 @@ test('has title', async ({ page }) => {
     await expect(page).toHaveTitle(/Register/);
 });
 
+test.describe('required fields', () => {
+    test('first name', async ({ page }) => {
+        await registrationPage.signUp('', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.firstnameerror).toBeVisible();
+    });
+    test('last name', async ({ page }) => {
+        await registrationPage.signUp('a', '', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.lastnameerror).toBeVisible();
+    });
+    test('address', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', '', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.addresserror).toBeVisible();
+    });
+    test('city', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', '', 'e', 'f', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.cityerror).toBeVisible();
+    });
+    test('state', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', '', 'f', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.stateerror).toBeVisible();
+    });
+    test('zipcode', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', 'e', '', 'g', 'a', 'a', 'a');
+        await expect(registrationPage.zipcodeerror).toBeVisible();
+    });
+    test('ssn', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', '', 'a', 'a', 'a');
+        await expect(registrationPage.ssnerror).toBeVisible();
+    });
+    test('username', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', '', 'a', 'a');
+        await expect(registrationPage.usernameerror).toBeVisible();
+    });
+    test('password', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', '', 'a');
+        await expect(registrationPage.passworderror).toBeVisible();
+    });
+    test('confirmation', async ({ page }) => {
+        await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', '');
+        await expect(registrationPage.confirmationerror).toBeVisible();
+    });
+  });
+
 test('this username already exists error', async ({ page }) => {
     // Create new user twice to create a duplicate
     await registrationPage.signUp('a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'a', 'a');
