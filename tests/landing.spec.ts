4f7ec8fb2c81b4197b5a0638749c93cf947b1d9b
Graeme Stewart <graemes88@hotmail.com>
2025-05-06 16:15:57 +0100

    Rename example to landing page

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -12,3 +12,9 @@ test('register link', async ({ page }) => {
   await page.getByRole('link', { name: 'Register' }).click();
   await expect(page).toHaveTitle(/Register/);
 });
+
+test('forgot login info link', async ({ page }) => {
+  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+  await page.getByRole('link', { name: 'Forgot login info?' }).click();
+  await expect(page).toHaveTitle(/Lookup/);
+});
